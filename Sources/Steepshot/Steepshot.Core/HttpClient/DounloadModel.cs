05e442c2beb0cd3cc5a7a0f4caec8da1d9cb8bf5
KorzunAV <KorzunAV@gmail.com>
2018-01-07 12:58:33 +0300

    download added (2)

---
+++ Sources/Steepshot/Steepshot.Core/HttpClient/DounloadModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Steepshot.Core.HttpClient
+{
+    public class DounloadModel : IDisposable
+    {
+        public Stream SourceStream { get; set; }
+
+        public long Total { get; set; }
+
+        public CancellationToken Token { get; set; }
+
+        public string MediaType { get; set; }
+
+        public void Dispose()
+        {
+            SourceStream?.Dispose();
+        }
+    }
+}
---
+++ Sources/Steepshot/Steepshot.Core/Models/Responses/DownloadResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Steepshot.Core.Models.Responses
+{
+    public class DownloadResponse
+    {
+    }
+}
