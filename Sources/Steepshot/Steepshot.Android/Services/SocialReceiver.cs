1637016ff5686e4892592896ee169fdd6cd4d73c
Maxim Matusevich <imulir@gmail.com>
2018-07-07 16:16:22 +0300

    Version for hockeyapp

--- Sources/Steepshot/Steepshot.Android/Services/SocialReceiver.cs
+++ Sources/Steepshot/Steepshot.Android/Services/SocialReceiver.cs
@@ -11,8 +11,8 @@ using Steepshot.Core.Utils;
 
 namespace Steepshot.Services
 {
-    [BroadcastReceiver]
-    [IntentFilter(new[] { Intent.ActionBootCompleted })]
+    [BroadcastReceiver(Enabled = true, Exported = true, Permission = "android.permission.RECEIVE_BOOT_COMPLETED", DirectBootAware = true)]
+    [IntentFilter(new[] { Intent.ActionBootCompleted, "android.intent.action.QUICKBOOT_POWERON" })]
     public class SocialReceiver : BroadcastReceiver
     {
         private TestPresenter presenter;
@@ -24,26 +24,21 @@ namespace Steepshot.Services
 
         public override void OnReceive(Context context, Intent intent)
         {
-            //var nm = (NotificationManager)context.GetSystemService(Context.NotificationService);
-            //int icon = Resource.Drawable.ic_steem;
-            //string text = "Test";
-            //long when = Java.Lang.JavaSystem.CurrentTimeMillis();
+            if (presenter.OpenApi.Gateway == null)
+                Log.Error("#Insta", $"Null gateway");
 
-            //var notification = new Notification(icon, text, when);
-
-            //var intentTL = new Intent(context, typeof(Activity.RootActivity));
-            //notification.SetLatestEventInfo(context, "Test", "Do something", PendingIntent.GetActivity(context, 0, intentTL, PendingIntentFlags.CancelCurrent));
-            //notification.Flags = NotificationFlags.AutoCancel;
-            //notification.Defaults = NotificationDefaults.Lights;
-            //nm.Notify(1, notification);
-
-            Log.Debug("#Debug", $"TaskStarted...");
-            Log.Debug("#Debug", $"Data: {presenter.OpenApi.Gateway.ToString()}, {AppSettings.User}");
-
-            var module = new InstagramModule(presenter.OpenApi.Gateway, AppSettings.User);
-            if (module.IsAuthorized())
+            var connectionService = AppSettings.ConnectionService;
+            if (connectionService.IsConnectionAvailable())
             {
-                module.TryCreateNewPost(CancellationToken.None);
+                var module = new InstagramModule(presenter.OpenApi.Gateway, AppSettings.User);
+                if (module.IsAuthorized())
+                {
+                    module.TryCreateNewPost(CancellationToken.None);
+                }
+            }
+            else
+            {
+                Log.Error("#Insta", $"No internet connection :(");
             }
 
             var am = (AlarmManager)context.GetSystemService(Context.AlarmService);
@@ -51,7 +46,7 @@ namespace Steepshot.Services
             var pIntent = PendingIntent.GetBroadcast(context, 0, myIntent, PendingIntentFlags.CancelCurrent);
             am.Set(AlarmType.RtcWakeup, Java.Lang.JavaSystem.CurrentTimeMillis() + 15000, pIntent);
 
-            Log.Debug("#Debug", $"TaskReceived! {module.IsAuthorized().ToString()}");
+            Log.Debug("#Insta", $"...taskReceived!");
         }
     }
 }
